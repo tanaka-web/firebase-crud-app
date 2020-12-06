d55f1fbb7df178d7802813807d1a595b83b1baa2
tanakaakira <tanaka.a.web@gmail.com>
2020-12-07 02:42:55 +0900

    delete code

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import App from '../components/App';
 
 const About: React.FC = () => (
   <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import App from '../components/App';
 
 const Home: React.FC = () => (
   <>
