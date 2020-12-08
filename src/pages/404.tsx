7bd2444a1baa7a777ab6a21cdbb932cf43097cf9
tanakaakira <tanaka.a.web@gmail.com>
2020-12-09 02:09:36 +0900

    create pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Custom404 = () => (
+  <div className="container">
+    <h3 className="text-center my-5">Page not found.</h3>
+    <div className="text-center">
+      <Link href="/">
+        <a>トップページへ</a>
+      </Link>
+    </div>
+  </div>
+);
+export default Custom404;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Home: React.FC = () => (
-  <>
-    <p>Index Page</p>
-  </>
+  <div className="container">
+    <h3 className="text-center my-5">一覧表示</h3>
+    <div className="my-3">
+      <Link href="/create">
+        <a>新規登録</a>
+      </Link>
+    </div>
+  </div>
 );
 
 export default Home;
