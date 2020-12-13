fa3357ebc946e16ea5324e483be98ca7754ddce3
tanakaakira <tanaka.a.web@gmail.com>
2020-12-14 03:41:36 +0900

    git rm firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'XXXXXXXX',
-  authDomain: 'XXXXXXXX',
-  projectId: 'XXXXXXXX',
-  storageBucket: 'XXXXXXXX',
-  messagingSenderId: 'XXXXXXXX',
-  appId: 'XXXXXXXX',
-  measurementId: 'XXXXXXXX',
-};
